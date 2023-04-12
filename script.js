6827d55dd728eb47f7f161dd5a6f7ef65373d2ae
fyip3 <89954179+fyip3@users.noreply.github.com>
2023-04-12 21:46:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const stocks = ["AAPL", "GOOGL", "MSFT", "AMZN", "FB", "TSLA", "NVDA", "NFLX", "PYPL", "ADBE"];
+const stocks = [
+  "AAPL", "GOOGL", "MSFT", "AMZN", "FB", "TSLA", "NVDA", "NFLX", "PYPL", "ADBE",
+  "BABA", "BAC", "BA", "C", "CAT", "CRM", "CSCO", "CVX", "DIS", "F",
+  "GE", "GILD", "GS", "HD", "IBM", "INTC", "JNJ", "JPM", "KO", "LMT",
+  "MCD", "MMM", "MRK", "NKE", "ORCL", "PEP", "PFE", "PG", "PM", "QCOM",
+  "RTX", "SBUX", "T", "TM", "UNH", "V", "VZ", "WBA", "WFC", "WMT",
+  "XOM", "AMGN", "AVGO", "BMY", "CELG", "CHTR", "COST", "CVS", "DHR", "DUK",
+  "EMR", "EXC", "GD", "GM", "GSK", "HON", "HPQ", "HSBC", "ITW", "KHC",
+  "LLY", "LUV", "MA", "MDT", "MMM", "MO", "MS", "NEE", "NVS", "NVZMY",
+  "PBR", "PCG", "PNC", "RDS-A", "RIO", "SAP", "SLB", "SNAP", "SNE", "SPOT",
+  "TSM", "TWTR", "UTX", "VOD", "WBA", "WFC", "X", "XRX", "YUM", "ZM"
+];
 
 const stockDisplay = document.getElementById("stock-name");
 const generateBtn = document.getElementById("generate-btn");
